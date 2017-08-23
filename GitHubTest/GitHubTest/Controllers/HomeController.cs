d1dd071bd63164297bff4b0107727f102598909f
Bartlomiej Balazy <Bartlomiej Balazy>
2017-08-23 02:22:28 +0200

    add Name property for register user form and class

--- GitHubTest/GitHubTest/Controllers/HomeController.cs
+++ GitHubTest/GitHubTest/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GitHubTest.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GitHubTest.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var gigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now).ToList();
+            return View(gigs);
         }
 
         public ActionResult About()
---
+++ GitHubTest/GitHubTest/Migrations/201708222356250_Add property name for applicationUser.cs
@@ -0,0 +1,18 @@
+namespace GitHubTest.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddpropertynameforapplicationUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String(nullable: false, maxLength: 100));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
